611aa4c7bfaa97bc88457db8d17b198dfd97e2d3
justnothin <vuleanhduc1604@gmail.com>
2023-05-10 22:36:46 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -24,5 +24,5 @@ const router = createBrowserRouter(routes);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
