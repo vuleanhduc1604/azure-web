caba0564413521d79421bf1abe7ce6db11a4c4d5
justnothin <vuleanhduc1604@gmail.com>
2023-06-20 10:42:30 +0700

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,22 @@ import { Outlet } from 'react-router'
 
 
 function App() {
+  const authenticate = () => new Promise((resolve) => setTimeout(resolve, 2000)) // 2 seconds
   useEffect(() => {
     window.scrollTo({top: 0, left: 0, behavior: 'smooth'})
+    authenticate().then(() => {
+      const ele = document.getElementById('ipl-progress-indicator')
+      if (ele) {
+        // fade out
+        ele.classList.add('available')
+        setTimeout(() => {
+          // remove from DOM
+          ele.outerHTML = ''
+        }, 2000)
+      }
+    })
   }, [])
+ 
   return (
     <div className="App">
       <Header />
